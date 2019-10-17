531963f31f4e4e07c58ca0ef2e8d98e792e0ce6c
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2019-10-17 18:43:01 -0300

    Inserindo usuário em lista do redux apenas para teste

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -1,10 +1,10 @@
 import {} from '../../../config/ReactotronConfig';
 export default function user(state, action) {
-  console.tron.log('teste: ', action);
+  console.tron.log('state: ', state);
 
   switch (action.type) {
     case 'ADD_USER':
-      return [];
+      return [...state, action.user];
     default:
       return [];
   }
